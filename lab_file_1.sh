b835d4def5c2d7891621f19aebb92081879d0f57
kokosikkkk <ssst.wvf@gmail.com>
2025-10-02 23:24:45 +0300

    first step

--- lab_file_1.sh
+++ lab_file_1.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-echo "my first script"
+echo "Вставте путь"
+path="$1"
+if [ $# -eq 0 ]; then
+	echo "Путь не введен"
+	exit 1
+fi
+
+if [ $# -gt 1 ]; then
+	echo "Проверьте, введен ли только путь"
+	exit 1
+fi
+
+if [ $# -z "$path" ]; then
+	echo "Введите путь"
+	exit 1
+fi
+if [! -e "$path"]; then
+	echo "Путь введен некоректно или такого путь не существует"
+	exit 1
+else
+	echo  "Идет проверка..."
